42d40a59a8aad27ce8c4865f9aa115810f60c273
imakoug <pats.s1889@gmail.com>
2024-11-07 19:33:43 +0100

    all info gathered, need 2 render properly

--- client/src/components/match/index.jsx
+++ client/src/components/match/index.jsx
@@ -1,14 +1,33 @@
+import dotaApiService from "../../services/dotaApi";
+import { useEffect, useState } from "react";
 
 function Match ({ match }) {
+
+  const [details, setDetails] = useState({});
+  const matchId = match.match_id;
+
+  useEffect(() => {
+    const getProfile = async () => {
+      let res = await dotaApiService.getMatch(matchId);
+      for (let player of res.players) {
+        if (player.hero_id === match.hero_id) {
+          setDetails(player);
+        }
+      }
+    };
+    getProfile();
+  }, [matchId]);
+
   return (
     <table>
       <tbody>
         <tr>
           <td>{match.match_id}</td>
-          <td>{match.game_mode === 22 ? "ranked" : "sd"}</td>
+          <td>3434</td>
           <td>
-            K/D/A - {match.kills}/{match.assists}/{match.deaths}
+            K/D/A - {match.kills}/{match.deaths}/{match.assists}
           </td>
+          <td>{details.account_id}</td>
         </tr>
       </tbody>
     </table>
